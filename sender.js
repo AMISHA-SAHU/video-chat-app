363c2c61c5e134f79d56802acdc629393fb1d2bd
AMISHA-SAHU <57638563+AMISHA-SAHU@users.noreply.github.com>
2021-07-11 08:09:28 +0530

    Add files via upload

---
+++ sender.js
@@ -0,0 +1,110 @@
+//websocket object
+const webSocket = new WebSocket("ws://127.0.0.1:3000")
+
+webSocket.onmessage = (event) => {
+    handleSignallingData(JSON.parse(event.data))
+}
+
+function handleSignallingData(data) {
+    switch (data.type) {
+        case "answer":
+            peerConn.setRemoteDescription(data.answer)
+            break
+        case "candidate":
+            peerConn.addIceCandidate(data.candidate)
+    }
+}
+//sendusername function so that socket will store that user name and storing it in globalvariable
+let username
+function sendRoomId() {
+
+    username = document.getElementById("input").value
+    sendData({
+        type: "store_user"
+    })
+}
+//sending data  we will sending it along with username so that server knows who the data belongs to
+function sendData(data) {
+    data.username = username
+    webSocket.send(JSON.stringify(data))
+}
+
+
+let localStream
+let peerConn
+//startcall function
+function startCall() {
+    document.getElementById("video")
+        //setting display to inline
+        .style.display = "inline"
+    //getting video stream using navigator
+    navigator.getUserMedia({
+        video: {
+            frameRate: 24,
+            width: {
+                min: 480, ideal: 720, max: 1280
+            },
+            aspectRatio: 1.3333
+        },
+        audio: true
+    }, (stream) => {
+        localStream = stream
+        document.getElementById("local").srcObject = localStream
+
+        let configuration = {
+            iceServers: [
+                {
+                    "urls": ["stun:stun.l.google.com:19302",
+                        "stun:stun1.l.google.com:19302",
+                        "stun:stun2.l.google.com:19302"]
+                }
+            ]
+        }
+        //peerconnection so that we can attach this localvideo when anyone connects stream is available to that person as well
+        peerConn = new RTCPeerConnection(configuration)
+        peerConn.addStream(localStream)
+
+        peerConn.onaddstream = (e) => {
+            document.getElementById("remote")
+                .srcObject = e.stream
+        }
+
+        peerConn.onicecandidate = ((e) => {
+            if (e.candidate == null)
+                return
+            sendData({
+                type: "store_candidate",
+                candidate: e.candidate
+            })
+        })
+
+        createAndSendOffer()
+    }, (error) => {
+        console.log(error)
+    })
+}
+
+function createAndSendOffer() {
+    peerConn.createOffer((offer) => {
+        sendData({
+            type: "store_offer",
+            offer: offer
+        })
+
+        peerConn.setLocalDescription(offer)
+    }, (error) => {
+        console.log(error)
+    })
+}
+
+let isAudio = true
+function muteAudio() {
+    isAudio = !isAudio
+    localStream.getAudioTracks()[0].enabled = isAudio
+}
+
+let isVideo = true
+function muteVideo() {
+    isVideo = !isVideo
+    localStream.getVideoTracks()[0].enabled = isVideo
+}
